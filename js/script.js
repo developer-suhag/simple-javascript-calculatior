5565f4c03c50b56c6825a9a050222ea3cdcf3daf
developer-suhag <developersuhag@gmail.com>
2021-08-17 21:26:26 +0600

    text size

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 const outputField = document.getElementById('output-field');
 
 document.getElementById('one').addEventListener('click', function () {
-    outputField.value += 1;
+    outputField.value += '1';
 });
 document.getElementById('two').addEventListener('click', function () {
-    outputField.value += 2;
+    outputField.value += '2';
 });
 document.getElementById('three').addEventListener('click', function () {
-    outputField.value += 3;
+    outputField.value += '3';
 });
 document.getElementById('four').addEventListener('click', function () {
-    outputField.value += 4;
+    outputField.value += '4';
 });
 document.getElementById('five').addEventListener('click', function () {
-    outputField.value += 5;
+    outputField.value += '5';
 });
 document.getElementById('six').addEventListener('click', function () {
-    outputField.value += 6;
+    outputField.value += '6';
 });
 document.getElementById('seven').addEventListener('click', function () {
-    outputField.value += 7;
+    outputField.value += '7';
 });
 document.getElementById('eight').addEventListener('click', function () {
-    outputField.value += 8;
+    outputField.value += '8';
 });
 document.getElementById('nine').addEventListener('click', function () {
-    outputField.value += 9;
+    outputField.value += '9';
 });
 document.getElementById('zero').addEventListener('click', function () {
-    outputField.value += 0;
+    outputField.value += '0';
 });
 document.getElementById('addition').addEventListener('click', function () {
     debugger;
